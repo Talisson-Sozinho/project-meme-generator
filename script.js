b5a92da2b24688b1ac1251e11af5459ed94a3d43
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-23 18:14:37 -0300

    feito - requisito 01 e 02 - passa no cypress:open porem não passa no npm test

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const textImage = document.getElementById('meme-text');
+const inputMemeImage = document.getElementById('meme-insert');
 const inputText = document.getElementById('text-input');
+const textImage = document.getElementById('meme-text');
+// const imageContainer = document.getElementById('meme-image-container');
 
-inputText.addEventListener('input', () => {
-  textImage.innerText = inputText.value;
+inputText.addEventListener('input', (event) => {
+  console.log(event.target.value);
+  console.log(textImage.innerHTML);
+  textImage.innerHTML = event.target.value;
+});
+
+inputMemeImage.addEventListener('input', (event) => {
+  const newImage = document.getElementById('meme-image');
+  const imageURL = URL.createObjectURL(event.target.files[0]);
+  newImage.setAttribute('src', imageURL);
 });
