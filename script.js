7d85c0ceb5e7cea3fe49124759538c8cebf3258e
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-23 21:08:25 -0300

    feito - requisito 6

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const inputMemeImage = document.getElementById('meme-insert');
 const inputText = document.getElementById('text-input');
 const textImage = document.getElementById('meme-text');
-// const imageContainer = document.getElementById('meme-image-container');
+const imageContainer = document.getElementById('meme-image-container');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
 
 inputText.addEventListener('input', (event) => {
   textImage.innerText = event.target.value;
@@ -11,4 +14,16 @@ inputMemeImage.addEventListener('input', (event) => {
   const newImage = document.getElementById('meme-image');
   const imageURL = URL.createObjectURL(event.target.files[0]);
   newImage.setAttribute('src', imageURL);
+});
+
+fireButton.addEventListener('click', () => {
+  imageContainer.style.border = '3px dashed red';
+});
+
+waterButton.addEventListener('click', () => {
+  imageContainer.style.border = '5px double blue';
+});
+
+earthButton.addEventListener('click', () => {
+  imageContainer.style.border = '6px groove green';
 });
